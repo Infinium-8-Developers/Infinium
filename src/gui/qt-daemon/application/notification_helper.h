5affce84f79310905064030c7318543db6fb98e1
Infinium-8-Developers <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-02-16 21:09:05 +0100

    Update notification_helper.h

--- src/gui/qt-daemon/application/notification_helper.h
+++ src/gui/qt-daemon/application/notification_helper.h
@@ -3,18 +3,24 @@
 #include "epee/include/misc_log_ex.h"
 
 #if defined(__APPLE__)
-    #include <Foundation/NSString.h>
+    #include <Foundation/Foundation.h>
 #endif
 
 struct notification_helper
 {
-  static void show(const std::string& title, const std::string& message)
-  {
-    #if defined(__APPLE__)
-
-    #else
-        // Implementación para otras plataformas
-        LOG_PRINT_RED("system notifications are not supported for this platform!", LOG_LEVEL_0);
-    #endif
-  }
+    static void show(const std::string& title, const std::string& message);
 };
+
+#ifdef __APPLE__
+void notification_helper::show(const std::string& title, const std::string& message)
+{
+
+}
+#else
+void notification_helper::show(const std::string& title, const std::string& message)
+{
+    // Implementación para otras plataformas
+    LOG_PRINT_RED("System notifications are not supported for this platform!", LOG_LEVEL_0);
+}
+#endif
+
