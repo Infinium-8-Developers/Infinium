c8cd4957557a53d6adb932d0f9003e12f4f1f25a
Infinium-8-Developers <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-02-17 13:02:31 +0100

    Update notification_helper.mm

--- src/gui/qt-daemon/application/notification_helper.mm
+++ src/gui/qt-daemon/application/notification_helper.mm
@@ -2,6 +2,7 @@
 #import <Foundation/Foundation.h>
 #import <Foundation/NSUserNotification.h>
 #import <Foundation/NSUserNotificationCenter.h>
+#import <Foundation/NSString.h>
 
 void notification_helper::show(const std::string& title, const std::string& message)
 {
