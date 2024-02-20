a724428f0e5638610f5c4df99d0ff101066361e9
Infinium-8-Developers <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-02-20 20:49:30 +0100

    Update notification_helper.mm

--- src/gui/qt-daemon/application/notification_helper.mm
+++ src/gui/qt-daemon/application/notification_helper.mm
@@ -1,10 +1,11 @@
 #include "notification_helper.h"
-#import <Foundation/Foundation.h>
-#import <Foundation/NSUserNotification.h>
-#import <Foundation/NSString.h>
+#include <Foundation/Foundation.h>
+#include <Foundation/NSUserNotification.h>
 
 void notification_helper::show(const std::string& title, const std::string& message)
 {
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
     NSUserNotification *userNotification = [[NSUserNotification alloc] init];
     [userNotification setTitle:[NSString stringWithUTF8String:title.c_str()]];
     [userNotification setInformativeText:[NSString stringWithUTF8String:message.c_str()]];
@@ -12,5 +13,5 @@ void notification_helper::show(const std::string& title, const std::string& mess
     NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
     [center deliverNotification:userNotification];
 
-    [userNotification release]; // Liberar el objeto
+    [pool release]; // Liberar el autorelease pool
 }
