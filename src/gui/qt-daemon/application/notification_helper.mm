5d5d6d305d1746b56bd69d5314e3917aa319a881
Infinium-8-Developers <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-02-21 14:46:02 +0100

    Update notification_helper.mm

--- src/gui/qt-daemon/application/notification_helper.mm
+++ src/gui/qt-daemon/application/notification_helper.mm
@@ -1,1 +1,14 @@
+#import "notification_helper.h"
+
+@implementation NotificationHelper
+
++ (void)showNotificationWithTitle:(NSString *)title message:(NSString *)message {
+    NSUserNotification *notification = [[NSUserNotification alloc] init];
+    notification.title = title;
+    notification.informativeText = message;
+    
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification:notification];
+}
+
+@end
 
