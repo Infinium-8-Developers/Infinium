89d36f29d1d5263ae0e1a5e0c843af47867cc048
Infinium-8-Developers <132166430+Infinium-8-Developers@users.noreply.github.com>
2024-02-21 15:23:15 +0100

    Update notification_helper.h

--- src/gui/qt-daemon/application/notification_helper.h
+++ src/gui/qt-daemon/application/notification_helper.h
@@ -13,5 +13,13 @@
         // Implementación para otras plataformas
         LOG_PRINT_RED("System notifications are not supported for this platform!", LOG_LEVEL_0);
     }
+#else
+    #import <Foundation/Foundation.h>
+    
+    @interface NotificationHelper : NSObject
+    
+    + (void)showNotificationWithTitle:(NSString *)title message:(NSString *)message;
+    
+    @end
 #endif
 
